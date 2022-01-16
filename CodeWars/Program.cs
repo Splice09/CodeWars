0a0534a72f3286045f1522c503527387ec7315cf
Patrick Fleming <pfleming@hylaine.com>
2022-01-15 23:09:14 -0500

    Adding vowel count code

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace CodeWars
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Kata.GetVowelCount("abebibobub");
+            Console.ReadLine();
+        }
+    }
+
+    public static class Kata
+    {
+        public static int GetVowelCount(string str)
+        {
+            int vowelCount = 0;
+
+            vowelCount = str.Where(c => c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u').Count();
+            Console.WriteLine(vowelCount);
+            return vowelCount;
         }
     }
 }
