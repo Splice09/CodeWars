4d70832b1c91ea36c1c9ef5c213b9a5da4cbeef1
Patrick Fleming <pfleming@hylaine.com>
2022-01-16 01:17:35 -0500

    completed phone numbers

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -6,7 +6,19 @@ namespace CodeWars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] phone = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            Kata.CreatePhoneNumber(phone);
+            Console.ReadLine();
+        }
+    }
+    public class Kata
+    {
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            string area = string.Join("", numbers[0..3]);
+            string exchange = string.Join("", numbers[3..6]);
+            string lineNum = string.Join("", numbers[6..10]);
+            return $"({area}) {exchange}-{lineNum}";
         }
     }
 }
