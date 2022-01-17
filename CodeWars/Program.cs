99797492c5dd138c30aad376c5bcca44791d3533
Patrick Fleming <pfleming@hylaine.com>
2022-01-16 23:21:41 -0500

    calculate odd occurrence

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -6,7 +6,21 @@ namespace CodeWars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Kata.find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 });
         }
+    }
+
+    class Kata
+    {
+        public static int find_it(int[] seq)
+        {
+            int oddOccurrence = 0;
+            for (int i = 0; i < seq.Length; i++)
+            {
+                oddOccurrence = oddOccurrence ^ seq[i];
+            }
+            return oddOccurrence;
+        }
+
     }
 }
