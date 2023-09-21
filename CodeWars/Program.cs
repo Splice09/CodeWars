c34e5e79310cf639d266dd9826012d3d50aa6aa4
Splice09 <jpf0005@gmail.com>
2023-09-21 11:01:08 -0400

    updating to .net core 6

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -5,8 +5,8 @@ namespace CodeWars
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {  
+            Console.WriteLine("Hello World!");  
         }
     }
 }
